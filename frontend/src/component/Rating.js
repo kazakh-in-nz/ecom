e0ee194a327bf31018b8f4e359443557991042b8
kazakh-in-nz <nursultanov.nurzhan@gmail.com>
2021-02-27 15:35:10 +1300

    Created Rating and Product components

---
+++ frontend/src/component/Rating.js
@@ -0,0 +1,27 @@
+import React from "react";
+
+function Rating(props) {
+  const { rating, numReviews } = props;
+  return (
+    <div className="rating">
+      <span>
+        <i className={rating >= 1 ? "fa fa-star" : rating >= 0.5 ? "fa fa-star-half-o" : "fa fa-star-o"}></i>
+      </span>
+      <span>
+        <i className={rating >= 1 ? "fa fa-star" : rating >= 0.5 ? "fa fa-star-half-o" : "fa fa-star-o"}></i>
+      </span>
+      <span>
+        <i className={rating >= 1 ? "fa fa-star" : rating >= 0.5 ? "fa fa-star-half-o" : "fa fa-star-o"}></i>
+      </span>
+      <span>
+        <i className={rating >= 1 ? "fa fa-star" : rating >= 0.5 ? "fa fa-star-half-o" : "fa fa-star-o"}></i>
+      </span>
+      <span>
+        <i className={rating >= 1 ? "fa fa-star" : rating >= 0.5 ? "fa fa-star-half-o" : "fa fa-star-o"}></i>
+      </span>
+      <span>{numReviews + " reviews"}</span>
+    </div>
+  );
+}
+
+export default Rating;
